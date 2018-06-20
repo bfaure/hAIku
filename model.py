254768ca832864487579b75a22184ff90f2a9351
Brian Faure <bfaure23@gmail.com>
2018-06-19 20:14:23 -0400

    initial version of LSTM model, running first tests

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
+print("\nInitializing...")
 print("Loading data... ",end="\r")
 filename="data.tsv"
 raw_text=open(filename,'r').read().lower()
@@ -21,5 +22,46 @@ n_vocab=len(chars)
 print("Total characters: %d"%n_chars)
 print("Total vocab: 	 %d"%n_vocab)
 
+# prepare the dataset of input to output pairs encoded as integers
+seq_length=9
+dataX=[]
+dataY=[]
+lines=raw_text.split('\n')
+total=0
+for haiku in lines:
+	haiku=haiku.strip()
+	for i in range(0, len(haiku)-1-seq_length):
+		total+=1
+		seq_in=haiku[i:i+seq_length]
+		seq_out=haiku[i+seq_length]
+		dataX.append([char_to_int[char] for char in seq_in])
+		dataY.append(char_to_int[seq_out])
+	print("\rTotal parsed: %d"%total,end="\r")
+
+n_patterns=len(dataX)
+print("\nInput size: %d"%(n_patterns))
+
+# creating one-hot mappings
+X=numpy.reshape(dataX,(n_patterns,seq_length,1))
+X=X/float(n_vocab)
+y=np_utils.to_categorical(dataY)
+
+# define LSTM model
+model=Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 
+# define the checkpoint
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+
+# fitting model
+print("Fitting model...")
+model.fit(X, y, nb_epoch=100, batch_size=128, callbacks=callbacks_list)
+
+print("Done.")
